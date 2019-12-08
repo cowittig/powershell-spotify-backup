dce16cab036d529bd44aed2716ee9161f50a9b38
Constantin Wittig <github@constantin-wittig.de>
2019-12-08 10:53:06 +0100

    #13 Created command Backup-SpotifyUserSongs to backup the user's favorite tracks.

---
+++ functions/Backup-SpotifyUserSongs.ps1
@@ -0,0 +1,59 @@
+function Backup-SpotifyUserSongs {
+    <#
+        .SYNOPSIS
+            Exports your favorite tracks from Spotify to a JSON file.
+
+        .DESCRIPTION
+            Exports your favorite tracks from Spotify to a JSON file. The tracks of the user are requested and 
+            the result is stored in a JSON file in the specified output directory or in the module base directory,
+            if no output directory is specified. A cached version will be used if the list of tracks has not changed.
+
+        .PARAMETER OutDir
+            Output directory. Default is the module root directory ($MyInvocation.MyCommand.Module.ModuleBase)
+
+        .INPUTS
+    	    None. You cannot pipe input to Backup-SpotifyUserSongs.
+
+        .OUTPUTS
+            A JSON file containing the users favorite songs in the specified or default output directory.
+
+        .EXAMPLE
+            PS> Backup-SpotifyUserSongs -OutDir C:\spotify-backups\
+    #>
+
+    [CmdLetBinding()]
+    param (
+        [string] $OutDir = $MyInvocation.MyCommand.Module.ModuleBase
+    )
+
+    if( -not (Test-Path $OutDir) ) {
+        mkdir $OutDir | Out-Null        # do not pollute output with mkdir output
+        Write-Information "Created output directory $OutDir"
+    }
+
+    $Token = Get-SpotifyValidToken
+
+    Write-Information 'Requesting user songs.'
+    $SongsRequestParams = @{
+        Uri = 'https://api.spotify.com/v1/me/tracks?offset=0&limit=50'
+        Method = 'GET'
+        Headers = @{ Authorization = "Bearer $Token" }
+    }
+    $Result = Get-SpotifyData -RequestParams $SongsRequestParams
+    $Songs = $Result.Items
+
+    while( $Result.Next ) {
+        Write-Information 'Requesting more user songs.'
+        $SongsRequestParams = @{
+            Uri = $Result.Next
+            Method = 'GET'
+            Headers = @{ Authorization = "Bearer $Token" }
+        }
+        $Result = Get-SpotifyData -RequestParams $SongsRequestParams
+        $Songs += $Result.Items
+    }
+
+    $OutFilePath = (Join-Path -Path $OutDir -ChildPath songs-backup.json)
+    $Songs | ConvertTo-Json -Depth 10 -Compress | Out-File $OutFilePath
+    Write-Information 'Created songs backup file.'
+}
--- powershell-spotify-backup.psd1
+++ powershell-spotify-backup.psd1
@@ -2,16 +2,18 @@
     ModuleVersion = '1.1.0'
     GUID = '9f4dfd05-124b-4665-a70f-236ec778bb08'
     Author = 'Constantin Wittig'
-    Description = 'Backup your Spotify playlists and albums in JSON format.'
+    Description = 'Backup your Spotify playlists, albums and songs in JSON format.'
 	PowerShellVersion = '5.1'
     NestedModules = 
         '.\functions\Set-SpotifyAccessTokens.ps1',
         '.\functions\Get-SpotifyValidToken.ps1',
         '.\functions\Get-SpotifyData.ps1',
         '.\functions\Backup-SpotifyUserPlaylists.ps1',
-        '.\functions\Backup-SpotifyUserAlbums.ps1'
+        '.\functions\Backup-SpotifyUserAlbums.ps1',
+        '.\functions\Backup-SpotifyUserSongs.ps1'
     FunctionsToExport =
         'Set-SpotifyAccessTokens',
         'Backup-SpotifyUserPlaylists',
-        'Backup-SpotifyUserAlbums'
+        'Backup-SpotifyUserAlbums',
+        'Backup-SpotifyUserSongs'
 }
